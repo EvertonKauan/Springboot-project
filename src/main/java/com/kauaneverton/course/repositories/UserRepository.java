970806c0cb447990e249f58afc8a79033e9b7fe3
EvertonKauan <evertonkauancg@gmail.com>
2022-08-30 11:20:26 -0300

    Service layer, component registration

--- src/main/java/com/kauaneverton/course/repositories/UserRepository.java
+++ src/main/java/com/kauaneverton/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.kauaneverton.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.kauaneverton.course.entities.User;
 
+@Repository // optional
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 	
