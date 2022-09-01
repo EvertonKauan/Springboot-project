b9d0e1f8ffb01258317f8009e1ff63b9919f5b02
EvertonKauan <evertonkauancg@gmail.com>
2022-09-01 11:57:00 -0300

    Comentary

--- src/main/java/com/kauaneverton/course/entities/Category.java
+++ src/main/java/com/kauaneverton/course/entities/Category.java
@@ -23,7 +23,7 @@ public class Category implements Serializable {
 	private String name;
 	
 	@Transient
-	private Set<Product> products = new HashSet<>();
+	private Set<Product> products = new HashSet<>(); //Association
 	
 	public Category() {
 	}
