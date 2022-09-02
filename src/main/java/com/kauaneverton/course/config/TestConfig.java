38d2ab1e0bca07c5d124a7977112a915581018c4
EvertonKauan <evertonkauancg@gmail.com>
2022-09-02 09:57:46 -0300

    Payment, one-to-one association

--- src/main/java/com/kauaneverton/course/config/TestConfig.java
+++ src/main/java/com/kauaneverton/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.kauaneverton.course.entities.Category;
 import com.kauaneverton.course.entities.Order;
 import com.kauaneverton.course.entities.OrderItem;
+import com.kauaneverton.course.entities.Payment;
 import com.kauaneverton.course.entities.Product;
 import com.kauaneverton.course.entities.User;
 import com.kauaneverton.course.entities.enums.OrderStatus;
@@ -82,5 +83,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
