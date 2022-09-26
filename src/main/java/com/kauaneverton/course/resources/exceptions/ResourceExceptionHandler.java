c03f0f73e00303cc92c333f6d53c2bea75f126cd
Everton Kauan <evertonkauancg@gmail.com>
2022-09-26 10:22:48 -0300

    Exception to remove users

--- src/main/java/com/kauaneverton/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/kauaneverton/course/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.kauaneverton.course.services.exceptions.DatabaseException;
 import com.kauaneverton.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -21,4 +22,11 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 }
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> NotFind(DatabaseException e, HttpServletRequest request) {
+		String error = "Error: The cliente have associated orders";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
 }
